43a2fa37d0f6780bf15526509360771d8153894c
sujan <bhattaraisujan9825@gmail.com>
2024-12-29 17:24:31 +0545

    Update

--- OOPs/5_InHeritance1.py
+++ OOPs/5_InHeritance1.py
@@ -2,17 +2,19 @@ class Person:
     def __init__(self,gender):
         self.gender=gender
 
-    def Display(self):
+    def get_gender(self):
         print(f"I am {self.gender}")
 
 class Student(Person):
-    def __init__(self,name):
+    def __init__(self,name,gender):
         self.name=name
+        # Person.__init__(self)
+        super().__init__(gender)
+
     def study(self):
         print(f"My Name is {self.name} and I am Studying")
 
-obj1=Person("Male")
-obj1.Display()
-# obj=Student("Ram")
-# obj.study()
-# obj.Display()
+obj=Student("Ram","male")
+obj.study()
+obj.get_gender()
+# print(obj.gender)
